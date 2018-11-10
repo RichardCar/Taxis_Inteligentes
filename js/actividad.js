9eb6a5f38191046e427faeec9b98a012b2bc8eca
Manuel Alvarado <cubosensei@gmail.com>
2018-11-10 13:44:05 -0500

    actividad: listo para prueba de usabilidad

--- js/actividad.js
+++ js/actividad.js
@@ -1,5 +1,5 @@
 d3.select(".div.vega-actions-wrapper").remove();
-var wh = d3.select("#lines").node().getBoundingClientRect().width-80;
+var wh = d3.select("#lines").node().getBoundingClientRect().width-100;
 var lines = {
   "data": {"url": "data/actividad.csv"},
   "mark": {"type":"line", "point":{"color":"black"}},
@@ -8,7 +8,14 @@ var lines = {
   "encoding": {
     "x": {"field": "h", "type": "quantitative", "axis": {"title": "Hora (24 horas)"}},
     "y": {"aggregate":"count","field": "estado", "type": "quantitative", "axis": {"title": "Estado (cantidad)"}},
-    "color": {"field": "estado","type": "nominal"}
+    "color": {
+      "field": "estado",
+      "type": "nominal",
+      "scale": {
+        "domain": ["null","ocupado","pedido"],
+        "range": ["#4c78a8","#f58518","#00a088"]
+      }
+    }
   },
 }
 
