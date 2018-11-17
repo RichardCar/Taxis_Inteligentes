d137824b4cb25289ac0b0f04f6b58fd68a6fec2e
Manuel Alvarado <cubosensei@gmail.com>
2018-11-17 16:54:51 -0500

    actividad: agregando etiquetas de ejes en gráfica de líneas

--- js/actividad.js
+++ js/actividad.js
@@ -20,9 +20,9 @@ var lines = {
 }
 
 vegaEmbed("#lines", lines);*/
-var margin = ({top: 20, right: 0, bottom: 20, left: 30});
+var margin = ({top: 20, right: 0, bottom: 35, left: 40});
 var wh = d3.select("#lines").node().getBoundingClientRect().width-10;
-var rawData, nulls, pedidos, ocupados, max, x, y, p, o, n, totalMediciones;
+var rawData, nulls, pedidos, ocupados, max, x, y, p, o, n, totalMediciones, taxis;
 var f_estado = "all";
 var f_desde = 0;
 var f_hasta = 23;
@@ -36,15 +36,23 @@ yAxis = g => g
   .attr("transform", `translate(${margin.left},0)`)
   .call(d3.axisLeft(y))
   .call(g => g.select(".domain").remove())
-  .call(g => g.select(".tick:last-of-type text").clone()
-    .attr("x", 3)
+  .call(g => g.select(".tick:first-of-type text").clone()
+    .attr("x", 10)
+    .attr("y", -30)
+    .style("transform","rotate(-90deg)")
     .attr("text-anchor", "start")
     .attr("font-weight", "bold")
     .text("Número de mediciones"));
 
 xAxis = g => g
   .attr("transform", `translate(0,${wh - margin.bottom})`)
-  .call(d3.axisBottom(x).ticks(wh / 80).tickSizeOuter(0));
+  .call(d3.axisBottom(x).ticks(wh / 80).tickSizeOuter(0))
+  .call(g => g.select(".tick:first-of-type text").clone()
+    .attr("y", 25)
+    .attr("x", 10)
+    .attr("text-anchor", "start")
+    .attr("font-weight", "bold")
+    .text("Hora del día"));
 
 loadData();
 
@@ -66,9 +74,15 @@ function loadData(){
     p = 0;
     o = 0;
     n = 0;
+    taxis = [];
 
     data.forEach(function(d, i){
       if(d.h >= f_desde && d.h <= f_hasta){
+        if(typeof taxis[d.tid] != "undefined"){
+          taxis[d.tid] += 1;
+        }else{
+          taxis[d.tid] = 1;
+        }
         if(d.estado == "pedido" && (f_estado == "all" || f_estado == "pedidos")){
           pedidos[d.h] += 1;
           p += 1;
@@ -93,7 +107,7 @@ function loadData(){
       }
     });
     totalMediciones = p+o+n;
-
+console.log(taxis);
     y = d3.scaleLinear()
       .domain([0, max])
       .range([ wh - margin.bottom, margin.top]);
