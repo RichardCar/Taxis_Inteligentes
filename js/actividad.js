eaadd191141ccb472907eff2e8ae6239607566e6
Manuel Alvarado <cubosensei@gmail.com>
2018-11-20 22:47:25 -0500

    actividad: refinar filtro y agregar animación al diagrama de líneas

--- js/actividad.js
+++ js/actividad.js
@@ -1,9 +1,13 @@
 var margin = ({top: 20, right: 0, bottom: 35, left: 40});
 var wh = d3.select("#lines").node().getBoundingClientRect().width-10;
 var rawData, mapData, etiquetas, nulls, pedidos, ocupados, max, x, y, p, o, n, totalMediciones, taxis;
-var f_estado = "all";
-var f_desde = 0;
-var f_hasta = 23;
+var f_all = 1;
+var f_pedidos = 0;
+var f_ocupados = 0;
+var f_nulls = 0;
+var nulls_old = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var ocupados_old = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var pedidos_old = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 line = d3.line()
   .defined(d => !isNaN(d))
@@ -13,7 +17,7 @@ line = d3.line()
 yAxis = g => g
   .attr("transform", `translate(${margin.left},0)`)
   .call(d3.axisLeft(y))
-  .call(g => g.select(".domain").remove())
+  //.call(g => g.select(".domain").remove())
   .call(g => g.select(".tick:first-of-type text").clone()
     .attr("x", 10)
     .attr("y", -30)
@@ -36,20 +40,21 @@ xAxis = g => g
 loadData();
 
 function loadData(){
+  if(f_pedidos == f_ocupados && f_ocupados == f_nulls && f_nulls == 1){
+    d3.select("#all").classed("is-selected", true);
+    d3.select("#pedidos").classed("is-selected", false);
+    d3.select("#ocupados").classed("is-selected", false);
+    d3.select("#nulls").classed("is-selected", false);
+    f_all = 1; f_pedidos = 0; f_ocupados = 0; f_nulls = 0;
+  }
   wh = d3.select("#lines").node().getBoundingClientRect().width-10;
   d3.csv("data/actividad.csv").then(function(data){
     rawData = data;
     etiquetas = ["12am","1am","2am","3am","4am","5am","6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm","8pm","9pm","10pm","11pm"];
 
-    if(f_estado == "all" || f_estado == "pedidos"){
-      pedidos = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-    }
-    if(f_estado == "all" || f_estado == "ocupados"){
-      ocupados = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-    }
-    if(f_estado == "all" || f_estado == "nulls"){
-      nulls = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-    }
+    pedidos = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    ocupados = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    nulls = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
     max = 0;
     p = 0;
@@ -58,27 +63,27 @@ function loadData(){
     taxis = [];
 
     data.forEach(function(d, i){
-      if(d.h >= f_desde && d.h <= f_hasta){
+      if(d.h >= 0 && d.h <= 23){
         if(typeof taxis[d.tid] != "undefined"){
           taxis[d.tid] += 1;
         }else{
           taxis[d.tid] = 1;
         }
-        if(d.estado == "pedido" && (f_estado == "all" || f_estado == "pedidos")){
+        if(d.estado == "pedido" && (f_all == 1 || f_pedidos == 1)){
           pedidos[d.h] += 1;
           p += 1;
           if(max < pedidos[d.h]){
             max = pedidos[d.h];
           }
         }
-        if(d.estado == "ocupado"  && (f_estado == "all" || f_estado == "ocupados")){
+        if(d.estado == "ocupado"  && (f_all == 1 || f_ocupados == 1)){
           ocupados[d.h] += 1;
           o += 1;
           if(max < ocupados[d.h]){
             max = ocupados[d.h];
           }
         }
-        if(d.estado == "null"  && (f_estado == "all" || f_estado == "nulls")){
+        if(d.estado == "null"  && (f_all == 1 || f_nulls == 1)){
           nulls[d.h] += 1;
           n += 1;
           if(max < nulls[d.h]){
@@ -93,10 +98,13 @@ function loadData(){
       .domain([0, max])
       .range([ wh - margin.bottom, margin.top]);
     x = d3.scaleLinear()
-      .domain([f_desde,f_hasta])
+      .domain([0,23])
       .range([margin.left, wh - margin.right]);
     
     drawLines();
+    nulls_old = nulls;
+    pedidos_old = pedidos;
+    ocupados_old = ocupados;
   });
 }
 
@@ -114,59 +122,95 @@ function drawLines(){
   svg.append("g")
     .call(yAxis);
 
-  if(f_estado == "all" || f_estado == "pedidos"){
-    svg.append("path")
-      .datum(pedidos)
-      .attr("fill", "none")
-      .attr("stroke", "#00a088")
-      .attr("stroke-width", 1.5)
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-      .attr("d", line);
-  }
-
-  if(f_estado == "all" || f_estado == "ocupados"){
-    svg.append("path")
-      .datum(ocupados)
-      .attr("fill", "none")
-      .attr("stroke", "#f58518")
-      .attr("stroke-width", 1.5)
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-      .attr("d", line);
-  }
-
-  if(f_estado == "all" || f_estado == "nulls"){
-    svg.append("path")
-      .datum(nulls)
-      .attr("fill", "none")
-      .attr("stroke", "#4c78a8")
-      .attr("stroke-width", 1.5)
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-      .attr("d", line);
-  }
+  scgAppendPath(pedidos, pedidos_old, "#00a088");
+  scgAppendPath(ocupados, ocupados_old, "#f58518");
+  scgAppendPath(nulls, nulls_old, "#4c78a8");
 
   d3.selectAll(".x-axis .tick text").each(function(d, i){
     d3.select(this).text(etiquetas[d]);
   });
+
+  function scgAppendPath(datos, datos_old, color){
+    svg.append("path")
+      .datum(datos)
+      .attr("fill", "none")
+      .attr("stroke", color)
+      .attr("stroke-width", 1.5)
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("d", line(datos_old))
+      .transition()
+      .duration(1000)
+      .attr("d", line);
+  }
 }
 
-d3.select("#estado")
-  .on("change", function(){
-    f_estado = d3.select(this).property("value");
+d3.select("#estado #all")
+  .on("click", function(){
+    if(!d3.select(this).classed("is-selected")){
+      d3.select(this).classed("is-selected", true);
+      d3.select("#pedidos").classed("is-selected", false);
+      d3.select("#ocupados").classed("is-selected", false);
+      d3.select("#nulls").classed("is-selected", false);
+      f_all = 1; f_pedidos = 0; f_ocupados = 0; f_nulls = 0;
+    }
     loadData();
   });
-d3.select("#desde")
-  .on("change", function(){
-    f_desde = d3.select(this).property("value");
+
+d3.select("#estado #pedidos")
+  .on("click", function(){
+    if(!d3.select(this).classed("is-selected")){
+      d3.select(this).classed("is-selected", true);
+      d3.select("#all").classed("is-selected", false);
+      f_all = 0; f_pedidos = 1;
+    }else{
+      d3.select(this).classed("is-selected", false);
+      f_pedidos = 0;
+
+      if(f_pedidos == f_ocupados && f_pedidos == f_nulls){
+        d3.select("#all").classed("is-selected", true);
+        f_all = 1;
+      }
+    }
     loadData();
   });
-d3.select("#hasta")
-  .on("change", function(){
-    f_hasta = d3.select(this).property("value");
+
+d3.select("#estado #ocupados")
+  .on("click", function(){
+    if(!d3.select(this).classed("is-selected")){
+      d3.select(this).classed("is-selected", true);
+      d3.select("#all").classed("is-selected", false);
+      f_all = 0; f_ocupados = 1;
+    }else{
+      d3.select(this).classed("is-selected", false);
+      f_ocupados = 0;
+
+      if(f_pedidos == f_ocupados && f_ocupados == f_nulls){
+        d3.select("#all").classed("is-selected", true);
+        f_all = 1;
+      }
+    }
     loadData();
   });
+
+d3.select("#estado #nulls")
+  .on("click", function(){
+    if(!d3.select(this).classed("is-selected")){
+      d3.select(this).classed("is-selected", true);
+      d3.select("#all").classed("is-selected", false);
+      f_all = 0; f_nulls = 1;
+    }else{
+      d3.select(this).classed("is-selected", false);
+      f_nulls = 0;
+
+      if(f_nulls == f_pedidos && f_nulls == f_ocupados){
+        d3.select("#all").classed("is-selected", true);
+        f_all = 1;
+      }
+    }
+    loadData();
+  });
+
 
 window.onresize = function(event) {
   loadData();
@@ -192,8 +236,8 @@ require([
     ready(function () {
         map = new Map("map", {
             basemap:"gray",
-            center:[-74.092092, 4.6509886],
-            zoom: 12
+            center:[-74.1082335, 4.6264527],
+            zoom: 17
         });
         addMarks();
     });
